11359ff99a64702fc31943c292c0b4bef7f0c95f
PresentKim <qjrmckd@gmail.com>
2018-04-22 14:07:44 +0900

    Add start.cmd
    https://gist.github.com/PresentKim/20b9c9e8529105691794d4d3d9f88825

---
+++ start.cmd
@@ -0,0 +1,27 @@
+@echo off
+TITLE PocketMine-MP server software for Minecraft: Pocket Edition
+cd /d %~dp0
+
+set PHPRC=""
+set MAIN_PATH=%~dp0
+set CORE_PATH=%MAIN_PATH%_core\
+set PHP_PATH=%CORE_PATH%php\
+set PHP_BINARY=%PHP_PATH%php.exe
+if exist %MAIN_PATH%PocketMine-MP.phar (
+    set POCKETMINE_FILE=%MAIN_PATH%PocketMine-MP.phar
+) else (
+    if exist %CORE_PATH%pmmp\ (
+        cd %CORE_PATH%pmmp\
+        %PHP_BINARY% %CORE_PATH%composer.phar install
+        cd %MAIN_PATH%
+
+        set POCKETMINE_FILE=%CORE_PATH%pmmp\src\pocketmine\PocketMine.php
+    ) else (
+        echo "Couldn't find a valid PocketMine-MP installation"
+        pause
+        exit 1
+    )
+)
+
+REM pause on exitcode != 0 so the user can see what went wrong
+%PHP_BINARY% -c %PHP_PATH% %POCKETMINE_FILE% %* || pause
