82cec5d5eb1022c8946a0d8816d87aad1932afb6
PresentKim <qjrmckd@gmail.com>
2018-08-20 20:56:21 +0900

    Update composer in `update_pmmp` instead of `start` and `start_loop`
    Don't have to do composer updates every time start the server.
    Only need to run it once to update the core file.

--- start_loop.cmd
+++ start_loop.cmd
@@ -2,21 +2,14 @@
 TITLE [WPDE] Start PMMP (LOOP)
 cd /d %~dp0
 
-set PHPRC=""
 set MAIN_PATH=%~dp0
 set CORE_PATH=%MAIN_PATH%_core\
-set PHP_PATH=%CORE_PATH%php\
-set PHP_BINARY=%PHP_PATH%php.exe
 
 ::Find PocketMine-MP core file
 if exist %MAIN_PATH%PocketMine-MP.phar (
     set POCKETMINE_FILE=%MAIN_PATH%PocketMine-MP.phar
 ) else (
     if exist %CORE_PATH%pmmp\ (
-        cd %CORE_PATH%pmmp\
-        %PHP_BINARY% %CORE_PATH%composer.phar install
-        cd %MAIN_PATH%
-
         set POCKETMINE_FILE=%CORE_PATH%pmmp\src\pocketmine\PocketMine.php
     ) else (
         echo "Couldn't find a valid PocketMine-MP installation"
@@ -24,6 +17,9 @@ if exist %MAIN_PATH%PocketMine-MP.phar (
         exit 1
     )
 )
+
+set PHPRC=""
+set PHP_PATH=%CORE_PATH%php\
 
 :loop
 ::Start PMMP
--- update_pmmp.cmd
+++ update_pmmp.cmd
@@ -9,3 +9,10 @@ set CORE_PATH=%MAIN_PATH%_core\
 cd %CORE_PATH%pmmp\
 git pull
 git submodule update --recursive
+
+:: Update composer
+set PHPRC=""
+set PHP_PATH=%CORE_PATH%php\
+cd %PMMP_PATH%
+%PHP_PATH%php.exe %CORE_PATH%composer.phar install
+cd %MAIN_PATH%
